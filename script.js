c5c3076f40fbd444dd9fcb530eabf7fc0a23d023
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-01 19:41:31 -0500

    add extra product and improve my project

--- script.js
+++ script.js
@@ -2,5 +2,45 @@ let cartCount = 0;
 
 function addToCart() {
     cartCount++;
+
     document.getElementById("cart-count").innerText = cartCount;
+
+    let notification =
+        document.getElementById("notification");
+
+    notification.style.display = "block";
+
+    setTimeout(() => {
+        notification.style.display = "none";
+    }, 2000);
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function searchProducts() {
+
+    let input =
+        document.getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    let products =
+        document.querySelectorAll(".product");
+
+    products.forEach(product => {
+
+        let title =
+            product.querySelector("h3")
+            .innerText
+            .toLowerCase();
+
+        if(title.includes(input)){
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+
+    });
 }
