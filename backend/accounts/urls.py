d7632d225d12c6431a19dec0c1f9cb4af8f46d6a
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-14 20:46:09 -0500

    Create urls.py

---
+++ backend/accounts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path(
+        "login/",
+        views.login_page,
+        name="login"
+    ),
+
+
+    path(
+        "logout/",
+        views.logout_page,
+        name="logout"
+    )
+
+]
