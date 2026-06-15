c7770acdb93d8e31e3cc06d5374bedc106d45525
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-14 20:46:24 -0500

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def login_page(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+
+        if user is not None:
+
+            login(request, user)
+
+            return redirect("/")
+
+
+    return render(request, "login.html")
+
+
+
+def logout_page(request):
+
+    logout(request)
+
+    return redirect("/")
