e28af3d6adf508d5463c277ac5b44f73884d55cb
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-15 21:10:35 -0500

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
 
-    path("", views.home, name="home"),
-
     path("login/", views.login_page, name="login"),
 
     path("logout/", views.logout_page, name="logout"),
+
+    path("", views.home, name="home"),
 
 ]
