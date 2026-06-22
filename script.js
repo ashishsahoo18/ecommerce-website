fe1e7b41ec919c1ce8e6bfaf265b709a33ca3c80
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-21 22:02:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1948,24 +1948,3 @@ askAI();
 
 
 });
-function voiceSearch(){
-
-let speech =
-new webkitSpeechRecognition();
-
-
-speech.lang="en-IN";
-
-
-speech.onresult=function(event){
-
-document.getElementById("user-message").value =
-event.results[0][0].transcript;
-
-
-}
-
-
-speech.start();
-
-}
